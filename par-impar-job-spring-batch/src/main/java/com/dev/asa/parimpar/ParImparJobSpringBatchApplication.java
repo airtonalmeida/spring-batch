03150d299ac80d5019799d302cc28b94a5b5f49e
airton.almeida <airton.almeida@fornecedores.goias.gov.br>
2022-11-12 21:53:55 -0300

    Primeiro commit

---
+++ par-impar-job-spring-batch/src/main/java/com/dev/asa/parimpar/ParImparJobSpringBatchApplication.java
@@ -0,0 +1,13 @@
+package com.dev.asa.parimpar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParImparJobSpringBatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParImparJobSpringBatchApplication.class, args);
+	}
+
+}
---
+++ par-impar-job-spring-batch/src/test/java/com/dev/asa/parimpar/ParImparJobSpringBatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev.asa.parimpar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParImparJobSpringBatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
