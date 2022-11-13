eab4d17369d851b216a595d5060303ab40c07281
airton.almeida <airton.almeida@fornecedores.goias.gov.br>
2022-11-12 22:11:13 -0300

    Primeiro commit

---
+++ primeiro-projeto-spring-batch/src/test/java/com/dev/asa/springbatch/PrimeiroProjetoSpringBatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev.asa.springbatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrimeiroProjetoSpringBatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
